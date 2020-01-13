b7da151eea735746113968d2937618bb821ffb32
Divn <Divakaran Sivasamy>
2020-01-13 12:37:04 +0530

    problem 1 solved

--- src/p1.clj
+++ src/p1.clj
@@ -1,5 +1,17 @@
 (ns p1)
 
-(defn -main []
-(println "(+ 2 3)"))
+(defn sol [n]
+(reduce + 
+(filter some?
+(for [x (range n)] 
+	(if (or (= 0 (mod x 3)) (= 0 (mod x 5))) x))))
+)
+
+
+(defn -main [] 
+	(println "hello")
+	(println "10 :" (sol 10))
+ (println "20 :" (sol 20))
+ (println "1000 :" (sol 1000))
+)
 
