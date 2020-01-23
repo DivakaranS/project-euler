31e6a0bd623c3d65b8ae04671fd7a970ff37c0fe
Divn <Divakaran Sivasamy>
2020-01-23 20:14:08 +0530

    p3 successful running.

--- src/p3.clj
+++ src/p3.clj
@@ -2,11 +2,12 @@
 
 
 (defn prime-factor [n]
-     (loop [i 2 arr []]
-      (if (and (<= i n) (not= n (apply * arr)))
-        (recur (inc i) (if (== 0 (mod n i)) (conj arr i) arr))
-        (apply max arr)
-     )))
+     (loop [f 2 facts [] prod n]
+      (if (and (<= f n) (not= n (apply * facts)))
+        (if (zero? (rem prod f))
+          (recur f (conj facts f) (quot prod f))
+          (recur (inc f) facts prod))
+        (apply max facts))))
 
 (defn -main []
   (reduce (fn [passing [input output]]
@@ -18,13 +19,15 @@
   		)))
   true 
   [
-			[[2] 2]
+		 [[2] 2]
 	  [[3] 3]
-	  ; [[4] 2]
+	  [[4] 2]
 	  [[5] 5]
-	  ; [[6] 3]
+	  [[6] 3]
 	  [[7] 7]
-	  ; [[8] 2]
+	  [[8] 2]
+	  [[9] 3]
+	  [[27] 3]
 	  [[13195] 29]
 	  [[600851475143] 6857]
   ]))
