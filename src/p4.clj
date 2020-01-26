32e56ff8440a7224b61c580e77de6e33dacf1656
Divn <Divakaran Sivasamy>
2020-01-26 15:41:09 +0530

    program 4 is refactored.

--- src/p4.clj
+++ src/p4.clj
@@ -1,18 +1,16 @@
 (ns p4)
 
-(defn palindrome [count]
- (if (== count 3)
-   (let [arr []]
-    (for [i (range 100 1000) j (range 100 1000)]
-      (let [n (* i j)]
-       (loop [val n final 0 temp n]
-      					(if (pos? val)
-        				(recur (quot val 10) (+ (* final 10) (mod val 10)) temp)
-         			(if (== final temp) final))))))))
-   
+(defn palindrome? [n]
+  (loop [value n  last 0]
+   (if (pos? value)
+    (recur (quot value 10) (+ (* last 10) (mod value 10)))
+     (== last n))))
+
+(defn largest-palindrome []
+  (apply max
+   (filter palindrome? 
+    (for [i (range 100 1000) j (range 100 1000)] (* i j)))))
 
 (defn -main [] 
-  (println "palindrome 3:" (apply max (filter (fn [n] (not (nil? n))) (palindrome 3))))
-  (println "palindrome 3:" (apply max (filter (comp not nil?) (palindrome 3))))
-		(println "palindrome 3:" (apply max (filter (comp not nil?) (palindrome 3)))))
+  (println "palindrome 3:" (largest-palindrome)))
 
